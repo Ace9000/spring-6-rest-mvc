6b3625c3c3a5a95e41b2f1736445a7778c35fabb
Anastas Acevski <anastas.acevski@outlook.com>
2023-05-17 11:38:26 +0200

    HTTP POST with Spring MVC

--- src/main/java/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/springframework/spring6restmvc/controller/BeerController.java
@@ -2,10 +2,10 @@ package springframework.spring6restmvc.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import springframework.spring6restmvc.model.Beer;
 import springframework.spring6restmvc.services.BeerService;
 
@@ -19,6 +19,14 @@ import java.util.UUID;
 public class BeerController {
     private final BeerService beerService;
 
+    //@RequestMapping(method = RequestMethod.POST)
+    @PostMapping()
+    public ResponseEntity handlePost(@RequestBody Beer beer) {
+
+        Beer savedBeer = beerService.saveNewBeer(beer);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
     @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers() {
         return beerService.listBeers();
--- src/main/java/springframework/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/springframework/spring6restmvc/services/BeerServiceImpl.java
@@ -69,4 +69,22 @@ public class BeerServiceImpl implements BeerService {
 
         return beerMap.get(id);
     }
+    @Override
+    public Beer saveNewBeer(Beer beer) {
+
+        Beer savedBeer = Beer.builder()
+                .id(UUID.randomUUID())
+                .createDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .beerName(beer.getBeerName())
+                .beerStyle(beer.getBeerStyle())
+                .quantityOnHand(beer.getQuantityOnHand())
+                .upc(beer.getUpc())
+                .price(beer.getPrice())
+                .build();
+
+        beerMap.put(savedBeer.getId(), savedBeer);
+
+        return savedBeer;
+    }
 }
